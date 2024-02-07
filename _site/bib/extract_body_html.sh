81cdf23a91636af51b8805b40b978ec008c71499
junishigohoka <junishigohoka@gmail.com>
2024-02-07 23:48:37 +0100

    edit

--- _site/bib/extract_body_html.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ls *html | while read infile
-do
-        outfile=`echo $infile | sed 's/.html/_html_body.txt/'`
-        awk '$0~"</body>"{print $0;exit}$0~"<body>"{c++}c==1{print $0}' $infile > $outfile
-done
-
